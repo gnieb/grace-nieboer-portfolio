9546fa7ab5fa6e786c33ba97f47610ca20525d0d
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-29 16:01:10 -0400

    fontawesome installed and configured

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ function Home () {
 
 
     return (
-        <div>
-            <h1 className = "font-bold" >Hello World</h1>
+        <div className="home">
+            <h1 className = "text-9xl" >HELLO World</h1>
+            <div>
+            <h1>get to know me</h1>
+            
+            </div>
+            
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,23 +4,29 @@ import { NavLink } from "react-router-dom";
 function NavBar () {
     return (
         <div>
-            <NavLink
-            to="/"
-            exact
-            >
-                Home
-            </NavLink>
-            <NavLink
-            to="/about"
-            exact
-            >
-                About
-            </NavLink>
-            <NavLink
-            to="/portfolio"
-            exact>
-                Portfolio
-            </NavLink>
+            <div>
+                <h1>GRACE NIEBOER</h1>
+                <h1>Software Engineer</h1>
+            </div>
+            <div>
+                <NavLink
+                to="/"
+                exact
+                >
+                    HOME
+                </NavLink>
+                <NavLink
+                to="/about"
+                exact
+                >
+                    ABOUT
+                </NavLink>
+                <NavLink
+                to="/portfolio"
+                exact>
+                    PORTFOLIO
+                </NavLink>
+            </div>
         </div>
     )
 }
