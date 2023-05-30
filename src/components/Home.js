0a706904b33bb260839820a9a92bd704d863142e
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-30 09:15:47 -0400

    instagram svg component created and added with link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ReactComponent as Instagram} from './instagram.svg'
 
 function Home () {
 
@@ -7,8 +8,10 @@ function Home () {
         <div className="home">
             <h1 className = "text-9xl" >HELLO World</h1>
             <div>
-            <h1>get to know me</h1>
-            
+                <h1>get to know me</h1>
+                <a href="https://www.instagram.com/grayce.elisabeth_/">
+                    <Instagram style={{height:"30px"}}/>
+                </a>
             </div>
             
         </div>
