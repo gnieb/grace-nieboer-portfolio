1fc2318e4aa83dcb2ecb15f592f9e0bb59630c85
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-30 14:56:36 -0400

    Contact component created, button created on home page, styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Contact from "./Contact";
 import {ReactComponent as Instagram} from './instagram.svg'
 import {ReactComponent as LinkedIn} from './linkedin-in.svg'
 
@@ -17,6 +18,15 @@ function Home () {
                 <a href="https://www.linkedin.com/in/gracenieboer/"> 
                     <LinkedIn style = {{height:"30px"}} />
                 </a>
+                <a>
+                    github
+                </a>
+                <a>
+                    blog
+                </a>
+            </div>
+            <div>
+                <button class="bg-transparent hover:bg-gray-500 text-white border border-gray-500 font-bold py-2 px-4 rounded-full">CONTACT ME</button>
             </div>
             
         </div>
