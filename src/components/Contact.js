987e0e8dc00a1be12fcfa071783a1ff9c7b08f6f
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-31 01:15:15 -0400

    add linkedin iconto contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import {ReactComponent as LinkedIn} from './linkedin-in.svg'
 import {ReactComponent as Envelope} from './envelope.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function Contact () {
     return (
@@ -11,11 +12,11 @@ export default function Contact () {
         </h1>
         Shoot me a message on LinkedIn or via Email
         <a href="https://www.linkedin.com/in/gracenieboer/" target="_blank">
-            <LinkedIn style={{height:"60px"}}/>
+        <FontAwesomeIcon icon={faLinkedin} style={{height:"60px",color: "#ffffff", margin:"10px"}}/>
         </a>
         <a href="mailto:grace.nieboer@outlook.com">
       
-            <FontAwesomeIcon icon={faEnvelope} style={{height:"60px"}}/>
+            <FontAwesomeIcon icon={faEnvelope} style={{height:"60px", color:"#ffffff", margin:"10px"}}/>
         </a>
         </>
     )
