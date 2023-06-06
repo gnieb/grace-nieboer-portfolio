7157570caabd974285305bc63338ce874d5e3361
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-05 23:28:49 -0400

    responsive CSS for mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,37 +10,41 @@ import { NavLink } from "react-router-dom";
 function Home () {
 
     return (
-        
-        <div className="grid grid-rows-3  gap-2">
-            <div className = "object-center row-span-2 col-span-3 text-[150px] lg:text-[250px] font-display font-extrabold space-y-0 ">
-                <p className="text-[30px]">creative architect</p>
-                <p>DEVELOPER</p>
+        <div className = "m-6 p-4">
+            <div className=" text-3xl sm:text-[40px]">
+                I AM A PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER. OH, AND I'M A COFFEE FIEND.
+            </div>
+            <div className = "object-center row-span-2 col-span-3  ">
+                <p className = "text-[30px] font-display">creative architect</p>
+                <p className="text-[150px] lg:text-[250px] font-display font-extrabold">DEV</p>
                 <p></p>
+                <NavLink
+                to="/contact"
+                exact
+                className="bg-transparent hover:bg-gray-300 text-white border text-2xl border-gray-500 font-bold py-4 px-8 rounded-full ">
+                    CONTACT ME
+                </NavLink>
             </div>
               
-            <div className="row-span-2">
-                <div className="  text-xl font-bold">get to know me</div>
-                    <a className="" href="https://www.instagram.com/grayce.elisabeth_/" >
+            <div className = "row-span-2 ">
+                <p className = "text-[30px] font-bold">get to know me</p>
+                    <a className="mx-2" href="https://www.instagram.com/grayce.elisabeth_/" >
                         <FontAwesomeIcon icon={faInstagram} style={{height:"30px", color: "#ffffff",}} />
                     </a>
-                    <a className="" href="https://www.linkedin.com/in/gracenieboer/"> 
+                    <a className="mx-2" href="https://www.linkedin.com/in/gracenieboer/"> 
                         <FontAwesomeIcon icon={faLinkedin} style={{height:"30px",color: "#ffffff",}}/>
                     </a>
-                    <a className="" href="https://github.com/gnieb">
+                    <a className="mx-2" href="https://github.com/gnieb">
                         <FontAwesomeIcon icon={faGithub} style={{height:"30px",color: "#ffffff",}} />
                     </a>
-                    <a className="" href="https://medium.com/@grace.nieboer">
+                    <a className="mx-2" href="https://medium.com/@grace.nieboer">
                         <FontAwesomeIcon icon={faMedium} style={{height:"30px",color: "#ffffff",}}/>
                     </a>
                 </div>
             <div className="">
-                <NavLink
-                to="/contact"
-                exact
-                className="bg-transparent hover:bg-gray-500 text-white border text-xl border-gray-500 font-bold py-4 px-8 rounded-full ">
-                    CONTACT ME
-                </NavLink>
+                
             </div>
+            
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,29 +6,29 @@ function NavBar () {
         <nav className="bg-transparent">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
-                    <div className="flex space-x-10">
-                        <h1 className="text-lg font-display font-bold">Grace Nieboer</h1>
-                        <h1 className="text-lg font-display font-bold">Software Engineer</h1>
+                    <div className="flex space-x-2 sm:space-x-10">
+                        <h1 className="text-sm sm:text-lg font-display font-bold">Grace Nieboer</h1>
+                        <h1 className="text-sm sm:text-lg font-display font-bold">Software Engineer</h1>
                     </div>
-                    <div className="flex space-x-4">
+                    <div className="flex space-x-1 sm:space-x-4">
                         <NavLink
                         to="/"
                         exact
-                        className="hover:text-white rounded-md px-3 py-2 text-xl font-medium"
+                        className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
                         >
                             HOME
                         </NavLink>
                         <NavLink
                         to="/about"
                         exact
-                        className="hover:text-white rounded-md px-3 py-2 text-xl font-medium"
+                        className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
                         >
                             ABOUT
                         </NavLink>
                         <NavLink
                         to="/portfolio"
                         exact
-                        className="hover:text-white rounded-md px-3 py-2 text-xl font-medium"
+                        className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
                         >
                             PORTFOLIO
                         </NavLink>
