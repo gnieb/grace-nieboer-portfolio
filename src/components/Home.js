9c7285bc29399d9497c34c6dfaaeb64241308e8d
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-13 14:27:08 -0400

    add flex box to home page and resizing for md screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,12 +11,9 @@ import Typewriter from "typewriter-effect";
 function Home () {
 
     return (
-        <div className = "m-6 p-4">
-            <div className=" text-3xl sm:text-[40px]">
-                I AM A PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER WITH A COFFEE IN MY HAND.
-            </div>
-            <div className = "text-[150px] text-left lg:text-[200px] font-display font-extrabold">
-                <p className = "text-[30px] text-center m-4 font-display">passionate</p>
+        <div className = "m-5 p-2">
+            <div className = "text-[50px] text-left md:text-[100px] xl:text-[200px] font-display font-extrabold">
+                <p className = "text-[20px] sm:text-[30px] text-left font-display">passionate</p>
                 <Typewriter
             
             options={{
@@ -25,35 +22,38 @@ function Home () {
                 loop: true,
               }}
             />
-                
             </div>
-              <div>
-                <NavLink
-                to="/contact"
-                exact
-                className=" bg-gray-300 hover:bg-orange-600 text-white border-2 text-xl sm:text-2xl border-white font-bold py-2 px-4 sm:py-4 sm:px-8 rounded-full ">
-                    CONTACT ME
-                </NavLink>
-                </div>
-            <div className = "">
-                <p className = "my-4 text-2xl sm:text-[30px] font-bold">get to know me</p>
-                    <a className="mx-2" href="https://www.instagram.com/grayce.elisabeth_/" >
-                        <FontAwesomeIcon icon={faInstagram} style={{height:"30px", color: "#ffffff",}} />
-                    </a>
-                    <a className="mx-2" href="https://www.linkedin.com/in/gracenieboer/"> 
-                        <FontAwesomeIcon icon={faLinkedin} style={{height:"30px",color: "#ffffff",}}/>
-                    </a>
-                    <a className="mx-2" href="https://github.com/gnieb">
-                        <FontAwesomeIcon icon={faGithub} style={{height:"30px",color: "#ffffff",}} />
-                    </a>
-                    <a className="mx-2" href="https://medium.com/@grace.nieboer">
-                        <FontAwesomeIcon icon={faMedium} style={{height:"30px",color: "#ffffff",}}/>
-                    </a>
-                </div>
-            <div className="">
+            <div className="flex justify-end">
                 
+                <div className=" text-3xl sm:text-5xl w-3/4 text-right ">
+                    I AM A PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER WITH A COFFEE IN MY HAND.
+                </div>
             </div>
-            
+            <div className = "flex flex-col text-right mr-28 my-20">
+                <div >
+                    <p className = "my-4 text-2xl sm:text-[40px] font-bold">get to know me</p>
+                        <a className="mx-2" href="https://www.instagram.com/grayce.elisabeth_/" >
+                            <FontAwesomeIcon icon={faInstagram} style={{height:"30px", color: "#ffffff",}} />
+                        </a>
+                        <a className="mx-2" href="https://www.linkedin.com/in/gracenieboer/"> 
+                            <FontAwesomeIcon icon={faLinkedin} style={{height:"30px",color: "#ffffff",}}/>
+                        </a>
+                        <a className="mx-2" href="https://github.com/gnieb">
+                            <FontAwesomeIcon icon={faGithub} style={{height:"30px",color: "#ffffff",}} />
+                        </a>
+                        <a className="mx-2" href="https://medium.com/@grace.nieboer">
+                            <FontAwesomeIcon icon={faMedium} style={{height:"30px",color: "#ffffff",}}/>
+                        </a>
+                </div>
+                <div className="my-12">
+                    <NavLink
+                    to="/contact"
+                    exact
+                    className=" hover:bg-gray-300 bg-orange-600 text-white border-2 text-xl sm:text-2xl border-white font-bold py-2 px-4 sm:py-4 sm:px-8 rounded-full mx-8 my-8 ">
+                        CONTACT ME
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 }
