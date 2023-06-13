ee2f5377ae7ebf307dec6d5d26d89525246d878b
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-13 14:49:22 -0400

    about summary basic

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,15 @@ function About () {
 
     return (
         <div> 
-            <h1>About Me</h1>
+            <p className="text-7xl">Hi there, and welcome! </p>
+            <p className = "text-4xl">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but lately I have also fallen in love with Python on the back end along with restful API architecture.</p>
+
+            <p className="text-3xl">BACKGROUND</p>
+            <p className="text-2xl">I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to work through employee issues and resolve conflicts. 
+             </p>
+
+             <p className="text-3xl">PERSON-FOCUSED</p>
+             <p className="text-xxl">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work with humans, to help other humans.</p>
         </div>
     )
 }
