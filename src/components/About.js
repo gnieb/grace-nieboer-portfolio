131e5e638ee7b50d813e8f0ff9f3645c94fb7bcb
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-13 15:13:39 -0400

    about background justify content styled for small screens and larger

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,17 @@
 function About () {
 
     return (
-        <div> 
-            <p className="text-7xl">Hi there, and welcome! </p>
-            <p className = "text-4xl">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but lately I have also fallen in love with Python on the back end along with restful API architecture.</p>
+        <div className="mx-8 relative"> 
+            <p className="text-7xl font-display my-5">HI THERE, AND WELCOME! </p>
+            <p className = "text-3xl text-justify sm:w-3/4 ">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with REST-ful API architecture.</p>
+            <div className="w-1/2 m-5 absolute right-0 mb-5">
+                <p className="text-3xl font-bold">BACKGROUND</p>
+                <p className="text-3xl text-justify">I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to work through employee issues and resolve conflicts. 
+                </p>
 
-            <p className="text-3xl">BACKGROUND</p>
-            <p className="text-2xl">I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to work through employee issues and resolve conflicts. 
-             </p>
-
-             <p className="text-3xl">PERSON-FOCUSED</p>
-             <p className="text-xxl">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work with humans, to help other humans.</p>
+                <p className="text-3xl font-bold mt-3">PERSON-FOCUSED</p>
+                <p className="text-3xl text-justify">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work with humans, to help other humans.</p>
+             </div>
         </div>
     )
 }
