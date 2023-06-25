09086d79386e5f8bc6e87e963531cfc316f7530a
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-25 19:36:23 -0400

    unique iframe titles

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -6,8 +6,8 @@ function ProjectPortfolio () {
     <>
     <h2 id="portfolio" className="text-4xl">Work</h2>
         <div >
-            <iframe src="https://www.loom.com/embed/d2cb126a6bbe4227b5eeaeba2bc0135b" style={{ width: "500px", height:"300px"}}></iframe>
-            <iframe src="https://www.loom.com/embed/7446ed88e32b49d7999592b0a3d95815" style={{width: "500px", height:"300px"}}></iframe>
+            <iframe title="inspiredint" src="https://www.loom.com/embed/d2cb126a6bbe4227b5eeaeba2bc0135b" style={{ width: "500px", height:"300px"}}></iframe>
+            <iframe title="hello" src="https://www.loom.com/embed/7446ed88e32b49d7999592b0a3d95815" style={{width: "500px", height:"300px"}}></iframe>
         </div>
     </>
     )
